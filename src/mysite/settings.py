3280a8312588ce8f063d77c081fd9c683d9daa73
sabrigan <pierre.dauphin@me.com>
2019-03-10 22:19:11 +0100

    tentative de reprise des templates de la section blog 29

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f4tmkc6#(q8k^9)p0q4n@op%hm%#&d%rr1%7sa07se^4jzcbfz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost","192.168.0.32","pierre-dauphin.hopto.org","127.0.0.1"]
 
