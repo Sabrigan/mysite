8a1c67d8e25e8c63ac8a99318d08fa0d2c614013
sabrigan <pierre.dauphin@me.com>
2019-04-14 13:11:19 +0200

    Modif de Timeline

--- src/blog/models.py
+++ src/blog/models.py
@@ -16,3 +16,23 @@ class Post(Timestamp):
 
     def __str__(self):
         return self.title
+
+
+class PostTimeline(Timestamp):
+    y_begin = models.IntegerField()
+    m_begin = models.IntegerField()
+    d_begin = models.IntegerField()
+    t_begin = models.TimeField()
+    y_end = models.IntegerField()
+    m_end = models.IntegerField()
+    d_end = models.IntegerField()
+    t_end = models.TimeField()
+    headline = models.CharField(max_length=500)
+    body = models.TextField()
+    media = models.URLField()
+    media_credit = models.CharField(max_length=250)
+    media_caption =
+    media_thumbnail =
+    type_tl = models.CharField()
+    group = models.CharField()
+    background = models.CharField()
