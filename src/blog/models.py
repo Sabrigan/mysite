6133215616e69623e838b3392f3aae23b0d619dc
sabrigan <pierre.dauphin@me.com>
2019-04-18 14:16:46 +0200

    Modif de Timeline

--- src/blog/models.py
+++ src/blog/models.py
@@ -18,21 +18,21 @@ class Post(Timestamp):
         return self.title
 
 
-class PostTimeline(Timestamp):
-    y_begin = models.IntegerField()
-    m_begin = models.IntegerField()
-    d_begin = models.IntegerField()
-    t_begin = models.TimeField()
-    y_end = models.IntegerField()
-    m_end = models.IntegerField()
-    d_end = models.IntegerField()
-    t_end = models.TimeField()
-    headline = models.CharField(max_length=500)
-    body = models.TextField()
-    media = models.URLField()
-    media_credit = models.CharField(max_length=250)
-    # media_caption =
-    # media_thumbnail =
-    type_tl = models.CharField()
-    group = models.CharField()
-    background = models.CharField()
+# class PostTimeline(Timestamp):
+#     y_begin = models.IntegerField()
+#     m_begin = models.IntegerField()
+#     d_begin = models.IntegerField()
+#     t_begin = models.TimeField()
+#     y_end = models.IntegerField()
+#     m_end = models.IntegerField()
+#     d_end = models.IntegerField()
+#     t_end = models.TimeField()
+#     headline = models.CharField(max_length=500)
+#     body = models.TextField()
+#     media = models.URLField()
+#     media_credit = models.CharField(max_length=250)
+#     # media_caption =
+#     # media_thumbnail =
+#     type_tl = models.CharField()
+#     group = models.CharField()
+#     background = models.CharField()
